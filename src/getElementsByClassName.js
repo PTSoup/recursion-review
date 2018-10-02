e3c5524e0746d680ecffd696422a62ad47392b12
Duncan Wallace <duncwa@gmail.com>
2018-10-02 10:33:03 -0700

    In progress recursion getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+// your code here
+// iterate over document comprised of tags with classnames (nested)
+// document arranged in a tree
+// iterate over the first node
+// iterate children of those nodes
+// when node has no descendants, stop iterating.
+// if className match with that element, add to results output
+// return the results output
+  var output = [];
+  var node = node || document.body;
+  var searchNode = function() {
+    node = node || document.body;
+    for (var i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].className && node.childNodes[i].className.toString().includes(className)) {
+        output.push(node.childNodes[i]);
+        if (node.childNodes[i].hasChildNodes()) {
+          searchNode(node.children[i]);
+        } else { 
+          return false;
+        }
+      } 
+    } 
+  };
+  searchNode();
+  return output;
 };
