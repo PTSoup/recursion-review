a6986ba82de24536901ca03868d697c6e7a70da4
Duncan Wallace <duncwa@gmail.com>
2018-10-02 18:04:31 -0700

    In progess.  getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+
+// document.body.childNodes = ['text', '<div> class='class'', '<div> class='stuff'']
+
+// getElementsbyClassName('class');
+
+var getElementsByClassName = function(className) { //=> 'class'
 // your code here
+
 // iterate over document comprised of tags with classnames (nested)
-// document arranged in a tree
-// iterate over the first node
-// iterate children of those nodes
-// when node has no descendants, stop iterating.
-// if className match with that element, add to results output
-// return the results output
-  var output = [];
+  // Evaluate if the element contains the given the className
+  // base case: 
+  // contains className
+  // then check to see if the element has children. 
+  // iterate over children of those nodes
+  // check to see if the child contains className
+  // if contains className, push to output
+  // if no children, end program and return output
+
+
+  var output = []; // [body.targetClassName]
   //var node = node || document.body;
   var searchNode = function(node) {
     node = node || document.body;
-    for (var i = 0; i < node.children.length; i++) {
-      if (node.children[i].className && node.children[i].className.toString().includes(className)) {
-        output.push(node.children[i]);
-        if (node.children[i].hasChildNodes) {
-          console.log('running if statement');
-          searchNode(node.children[i]);
-        } else { 
-          return false;
-        }
+    for (var i = 0; i < node.length; i++) {
+      if (node.className.toString().includes(className)) {
+        console.log('this code is accessible');
+        console.log(output);
+        output.push(node[i]); // node
+        // if (node.children[i].hasChildNodes)  {
+        //   console.log('running if statement');
+        //   searchNode(node.children[i]);
+        // } else { 
+        //   return false;
+        // }
       } 
     } 
   };
@@ -33,3 +45,10 @@ var getElementsByClassName = function(className) {
   return output;
 };
 
+// output = [body.targetClassName, div.targetClassName]
+// body - targetClassName - > body.targetClassName
+  // div
+  // div - targetClassName -> div.targetClassName
+  // div
+  // div
+  // div
